b7dbf415edeed5dadeca98743b9ba1d3ca3a8eff
派大星 <l18866432310@163.com>
2021-10-17 20:53:22 +0800

    eesrser

---
+++ src/components/Student.vue
@@ -0,0 +1,18 @@
+<template>
+  <dev>
+    <h2>学生姓名：{{name}}</h2>
+    <h2>学生年龄：{{age}}</h2>
+  </dev>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data(){
+    return{
+      name:'张',
+      age:18
+    }
+  }
+}
+</script>
